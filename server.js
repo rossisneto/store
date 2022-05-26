51c2bb5e2061da9210015ebec43ab7473332ac7f
RS Neto <rossi.neto@gmail.com>
2022-05-26 15:50:45 -0300

    inclusao da rota produtos

--- server.js
+++ server.js
@@ -9,4 +9,8 @@ app.get('/', (req,res)=>{
     res.end("Servidor ativo")
 })
 
+app.get('/produtos', (req,res)=>{
+    res.end("Esta eh a nossa pagina de produtos")
+})
+
 app.listen(process.env.PORT, console.log("servidor iniciado"))
